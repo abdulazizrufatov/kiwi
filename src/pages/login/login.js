478e196966f47dfb37ea4d488d832b595b32117e
Abdulaziz Rufatov <103237265+abdulazizrufatov@users.noreply.github.com>
2022-10-11 23:08:36 +0500

    initial commit

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useRef } from "react";
 import { Header } from "../../components/header/header";
 import { LangContext } from "../../context/langContext";
 import { Lang } from "../../languages/language";
